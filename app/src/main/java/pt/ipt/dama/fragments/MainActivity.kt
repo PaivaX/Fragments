4e468f49a64c79627431a945e59be723863dfdc0
Rui Paiva <rui.paiva88@gmail.com>
2023-05-17 21:48:48 +0100

    2nd commit

--- app/src/main/java/pt/ipt/dama/fragments/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/fragments/MainActivity.kt
@@ -5,7 +5,24 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // create the objets of type 'fagment' to use
+        lateinit var f1:MyFragment
+        lateinit var f2:MyFragment
+        lateinit var f3:MyFragment
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // init the fragments
+        f1= MyFragment.newInstance("fragment","one")
+        f2= MyFragment.newInstance("Fragment","Two")
+        f3= MyFragment.newInstance("FRAGMENT", "THREE")
+
+        val fragmentTransaction=supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment1, f1)
+        fragmentTransaction.add(R.id.fragment2, f2)
+        fragmentTransaction.add(R.id.fragment3, f3)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
